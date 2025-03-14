84c74ebc4d2fe94a89d1e96f6b6a6a031a63fbe2
ImLJS <109210868+ImLJS@users.noreply.github.com>
2025-03-14 16:31:48 +0530

    Remove Tech Skills

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Hero from '@/components/Hero';
-import TechSkills from '@/components/TechSkills';
 
 const HomePage = () => {
   return (
     <>
       <Hero />
-      <TechSkills />
     </>
   );
 };
